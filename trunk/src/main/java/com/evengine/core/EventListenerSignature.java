5ec681d1017204a369dacb2330565bbd9e4eab7f
sumeet.chhetri <sumeet.chhetri@dd244076-0e5a-cb43-4f30-36e45bec7193>
2013-08-26 13:07:02 +0000

    Added the new EventType annotation for idempotent eevent feature Updated other reference classes Added a findDuplicate method to persistent store to check whether a duplicate event already exists

--- trunk/src/main/java/com/evengine/core/EventListenerSignature.java
+++ trunk/src/main/java/com/evengine/core/EventListenerSignature.java
@@ -38,4 +38,83 @@ public class EventListenerSignature implements Serializable
     {
         return event;
     }
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((dispatchDate == null) ? 0 : dispatchDate.hashCode());
+        result = prime * result + ((error == null) ? 0 : error.hashCode());
+        result = prime * result + ((event == null) ? 0 : event.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + (isDone ? 1231 : 1237);
+        result = prime * result + ((listenerClassName == null) ? 0 : listenerClassName.hashCode());
+        result = prime * result + ((listenerMethodName == null) ? 0 : listenerMethodName.hashCode());
+        result = prime * result + ((proessedDate == null) ? 0 : proessedDate.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        EventListenerSignature other = (EventListenerSignature) obj;
+        if (dispatchDate == null)
+        {
+            if (other.dispatchDate != null)
+                return false;
+        }
+        else if (!dispatchDate.equals(other.dispatchDate))
+            return false;
+        if (error == null)
+        {
+            if (other.error != null)
+                return false;
+        }
+        else if (!error.equals(other.error))
+            return false;
+        if (event == null)
+        {
+            if (other.event != null)
+                return false;
+        }
+        else if (!event.equals(other.event))
+            return false;
+        if (id == null)
+        {
+            if (other.id != null)
+                return false;
+        }
+        else if (!id.equals(other.id))
+            return false;
+        if (isDone != other.isDone)
+            return false;
+        if (listenerClassName == null)
+        {
+            if (other.listenerClassName != null)
+                return false;
+        }
+        else if (!listenerClassName.equals(other.listenerClassName))
+            return false;
+        if (listenerMethodName == null)
+        {
+            if (other.listenerMethodName != null)
+                return false;
+        }
+        else if (!listenerMethodName.equals(other.listenerMethodName))
+            return false;
+        if (proessedDate == null)
+        {
+            if (other.proessedDate != null)
+                return false;
+        }
+        else if (!proessedDate.equals(other.proessedDate))
+            return false;
+        return true;
+    }
+    
 }
